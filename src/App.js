738343e3b052e4a4eabc1bf062ec23bc931d9e72
gohain1996 <flowerdark777@gmail.com>
2022-03-05 14:39:54 +0530

    my spa project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import First  from'./components/First';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <First/> */}
+    <Hello/>
+    </div>
+  );
+}
+
+export default App;
